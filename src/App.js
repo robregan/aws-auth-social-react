6e0cd960c52af5ef00ad6c3b4b348fa63156ec7e
rob <robert_regan86@yahoo.com>
2022-01-30 23:20:58 -0500

    added login with facebook, google, and amazon via aws

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import Game from './components/Game'
+import { withAuthenticator, useAuthenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css'; // default theme
+import Auth from 'aws-amplify'
+import aws_exports from './aws-exports'
+Auth.configure(aws_exports)
 
+const App = () => {
+ const { signOut } = useAuthenticator()
+ const styles = {
+  width: '200px',
+  margin: '0 auto',
+  alignItems: 'center',
+};
 
-
-const App = () => 
-  (
-    <>
-      <Game />
-    </>
-  ) 
-
-
-export default App;
+ return(
+   <>
+  <Game />
+  <button styles={styles} onClick={signOut}>Sign Out</button>
+  </>
+ ) 
+}
+export default withAuthenticator(App);
